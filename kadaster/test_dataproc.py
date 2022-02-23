4e6b7b6f88b7e35780158f3a6cab38b0e7331308
Marc Zoon <marc.zoon@vintus.nl>
2022-02-23 13:53:09 +0100

    added columns with converted geospatial data
    Where applicable, there is now a geography column. Geospatial data is represented in the WKT format.

--- kadaster/test_dataproc.py
+++ kadaster/test_dataproc.py
@@ -1,11 +1,16 @@
 import logging
 import os
-from os import listdir, remove
+from os import remove
 from sys import stdout
 from zipfile import ZipFile
 
 import requests
+from pyspark.sql.types import *
 from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, udf, struct
+from shapely import geometry, wkt
+
+from pyproj import Proj, Transformer, transform
 
 
 # logging
@@ -31,8 +36,8 @@ DATA_DIR = f'{CWD}/data'
 
 
 # GCP
-TMP_BUCKET = 'temp-data-kadaster'
-DATASET = 'testing'
+TMP_BUCKET = 'temp-data-pyspark'
+DATASET = 'bag'
 
 
 # Object maps
@@ -44,29 +49,6 @@ object_map = {
     'STA': 'Standplaats',
     'PND': 'Pand',
     'VBO': 'Verblijfsobject',
-    'IAWPL': 'Woonplaats_IA',
-    'IAOPR': 'OpenbareRuimte_IA',
-    'IANUM': 'Nummeraanduiding_IA',
-    'IALIG': 'Ligplaats_IA',
-    'IASTA': 'Standplaats_IA',
-    'IAPND': 'Pand_IA',
-    'IAVBO': 'Verblijfsobject_A',
-    'NBWPL': 'Woonplaats_NB',
-    'NBOPR': 'OpenbareRuimte_NB',
-    'NBNUM': 'Nummeraanduiding_NB',
-    'NBLIG': 'Ligplaats_NB',
-    'NBSTA': 'Standplaats_NB',
-    'NBPND': 'Pand_NB',
-    'NBVBO': 'Verblijfsobject_NB',
-}
-inonderzoek_map = {
-    'IOWPL': 'KenmerkWoonplaatsInOnderzoek',
-    'IOOPR': 'KenmerkOpenbareRuimteInOnderzoek',
-    'IONUM': 'KenmerkNummeraanduidingInOnderzoek',
-    'IOLIG': 'KenmerkLigplaatsInOnderzoek',
-    'IOSTA': 'KenmerkStandplaatsInOnderzoek',
-    'IOPND': 'KenmerkPandInOnderzoek',
-    'IOVBO': 'KenmerkVerblijfsobjectInOnderzoek',
 }
 
 
@@ -75,6 +57,7 @@ def download_bag_zip():
     r = requests.get(BAG_URL)
     with open(BAG_FILE, 'wb') as f:
         f.write(r.content)
+
 
 def init_spark_session():
     """
@@ -100,6 +83,7 @@ def init_spark_session():
                    "CORRECTED")
     spark.conf.set("spark.sql.legacy.parquet.datetimeRebaseModeInWrite",
                    "CORRECTED")
+
     return spark
 
 
@@ -127,24 +111,30 @@ def unzip_bag_data():
                      for f in zip.namelist() if f.endswith('.xml')]
         file_dict[name.split('.')[0]] = files
         remove(f'{DATA_DIR}/{name}')
-
-    # extract zips on depth 2 (InOnderzoek, Inactief, NietBAG)
-    for directory in listdir(DATA_DIR):
-        dir_no_digit = ''.join([c for c in directory if not c.isdigit()])
-        if dir_no_digit in ['InOnderzoek', 'Inactief', 'NietBag']:
-            zipnames = listdir(f'{DATA_DIR}/{directory}')
-            for name in zipnames:
-                if not name.endswith('.zip'):
-                    continue
-                logging.info(f'Extracting {name}')
-                with ZipFile(f'{DATA_DIR}/{directory}/{name}') as zip:
-                    zip.extractall(f"{DATA_DIR}/{directory}/{name.split('.')[0]}")
-                    files = [f"{DATA_DIR}/{directory}/{name.split('.')[0]}/{f}"
-                             for f in zip.namelist() if f.endswith('.xml')]
-                file_dict[name.split('.')[0]] = files
-                remove(f'{DATA_DIR}/{directory}/{name}')
-
     return file_dict
+
+
+def transform_coords(x, y):
+    x, y = transformer.transform(x, y)
+    return [x, y]
+
+
+def convert_punt(s: str):
+    x, y, _ = s.split()
+    x, y = transform_coords(x, y)
+
+    p = wkt.dumps(geometry.Point(x, y))
+    return p
+
+
+def create_linear_ring(s: str, dimension=2):
+    points_list = []
+    s = s.split()
+    for i in range(0, len(s), dimension):
+        x, y = transform_coords(s[i], s[i+1])
+        points_list.append([x, y])
+    lr = geometry.LinearRing(points_list)
+    return lr
 
 
 def process_table(object_type, files, root_tag, row_tag, select_tag):
@@ -156,27 +146,157 @@ def process_table(object_type, files, root_tag, row_tag, select_tag):
         files (str): A string containing all files to process. Joined using
             a comma. (i.e. 'file1,file2')
     """
-    try:
-        logging.info(f'Processing {object_type} xml files')
+    logging.info(f'Processing {object_type} xml files')
 
-        df = spark.read.format('xml') \
-            .option('rootTag', root_tag) \
-            .option('rowTag', row_tag) \
-            .option('path', files) \
-            .load()
-        df = df.select(f"{select_tag}:{object_type.split('_')[0]}.*")
-        df.write \
-            .format('bigquery') \
-            .option('table', f'{DATASET}.{object_type}') \
-            .mode('overwrite') \
-            .save()
-    except:
-        logging.warn(f'Unable to load {object_type} files.')
+    df = spark.read.format('xml') \
+        .option('rootTag', root_tag) \
+        .option('rowTag', row_tag) \
+        .option('path', files) \
+        .load()
+    df = df.select(f"{select_tag}:{object_type.split('_')[0]}.*")
+    if 'Objecten:geometrie' in df.columns:
+        new_geo_cols = []
+        geo_df = df.select('Objecten:geometrie.*')
+
+        if 'Objecten:multivlak' in geo_df.columns:
+            new_multi_polygons = []
+
+            for row in df.collect():
+                id = row['Objecten:identificatie']['_VALUE']
+                voorkomen = row['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie']
+
+                if row['Objecten:geometrie']['Objecten:multivlak'] is not None:
+                    multi_surface = row['Objecten:geometrie']['Objecten:multivlak']['gml:MultiSurface']['gml:surfaceMember']
+                    polygons = []
+                    for surface in multi_surface:
+                        # exterior
+                        positions = surface['gml:Polygon']['gml:exterior']['gml:LinearRing']['gml:posList']['_VALUE']
+                        exterior_lr = create_linear_ring(positions)
+
+                        # interiors
+                        interiors = surface['gml:Polygon']['gml:interior']
+                        interior_lrs = []
+                        if interiors is not None:
+                            for interior in interiors:
+                                positions = interior['gml:LinearRing']['gml:posList']['_VALUE']
+                                interior_lr = create_linear_ring(positions)
+                                interior_lrs.append(interior_lr)
+                        polygon = geometry.Polygon(exterior_lr, interior_lrs)
+                        polygons.append(polygon)
+                    multipolygon = wkt.dumps(geometry.MultiPolygon(polygons))
+                    new_multi_polygons.append((id, voorkomen, multipolygon))
+
+            new_df = spark.createDataFrame(new_multi_polygons, ['id', 'voorkomen', 'geo_multi_polygon'])
+
+            cond = [
+                df['Objecten:identificatie']['_VALUE'] == new_df.id,
+                df['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie'] == new_df.voorkomen
+            ]
+
+            new_geo_cols.append('geo_multi_polygon')
+            df = df.join(new_df, cond, 'full').drop('id').drop('voorkomen')
+
+
+        if 'Objecten:vlak' in geo_df.columns:
+            polygons = []
+            for row in df.collect():
+                id = row['Objecten:identificatie']['_VALUE']
+                voorkomen = row['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie']
+
+                if row['Objecten:geometrie']['Objecten:vlak'] is not None:
+                    # exterior
+                    positions = row['Objecten:geometrie']['Objecten:vlak']['gml:Polygon']['gml:exterior']['gml:LinearRing']['gml:posList']['_VALUE']
+                    exterior_lr = create_linear_ring(positions)
+
+                    # interiors
+                    interior = row['Objecten:geometrie']['Objecten:vlak']['gml:Polygon']['gml:interior']
+                    interior_lrs = []
+                    if interior is not None:
+                        positions = interior['gml:LinearRing']['gml:posList']['_VALUE']
+                        interior_lr = create_linear_ring(positions)
+                        interior_lrs.append(interior_lr)
+
+                    polygon = geometry.Polygon(exterior_lr, interior_lrs)
+                    polygons.append((id, voorkomen, wkt.dumps(polygon)))
+
+            new_df = spark.createDataFrame(polygons, ['id', 'voorkomen', 'geo_polygon'])
+
+            cond = [
+                df['Objecten:identificatie']['_VALUE'] == new_df.id,
+                df['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie'] == new_df.voorkomen
+            ]
+
+            new_geo_cols.append('geo_polygon')
+            df = df.join(new_df, cond, 'full').drop('id').drop('voorkomen')
+
+        if 'Objecten:punt' in geo_df.columns:
+            points = []
+            for row in df.collect():
+                id = row['Objecten:identificatie']['_VALUE']
+                voorkomen = row['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie']
+                point = row['Objecten:geometrie']['Objecten:punt']['gml:Point']['gml:pos']
+                points.append((id, voorkomen, convert_punt(point)))
+
+            points_df = spark.createDataFrame(points, ['id', 'voorkomen', 'geo_point'])
+
+            cond = [
+                df['Objecten:identificatie']['_VALUE'] == points_df.id,
+                df['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie'] == points_df.voorkomen
+            ]
+            df = df.join(points_df, cond, 'full')
+            df = df.drop('id').drop('voorkomen')
+            new_geo_cols.append('geo_point')
+
+        if 'gml:Polygon' in geo_df.columns:
+            polygons = []
+            for row in df.collect():
+                id = row['Objecten:identificatie']['_VALUE']
+                voorkomen = row['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie']
+
+                if row['Objecten:geometrie']['gml:Polygon'] is not None:
+                    dimension = row['Objecten:geometrie']['gml:Polygon']['_srsDimension']
+                    # exterior
+                    positions = row['Objecten:geometrie']['gml:Polygon']['gml:exterior']['gml:LinearRing']['gml:posList']['_VALUE']
+                    exterior_lr = create_linear_ring(positions, dimension)
+
+                    # interiors
+                    interiors = row['Objecten:geometrie']['gml:Polygon']['gml:interior']
+                    interior_lrs = []
+                    if interiors is not None:
+                        for interior in interiors:
+                            positions = interior['gml:LinearRing']['gml:posList']['_VALUE']
+                            interior_lr = create_linear_ring(positions, dimension)
+                            interior_lrs.append(interior_lr)
+
+                    polygon = wkt.dumps(geometry.Polygon(exterior_lr, interior_lrs))
+                    polygons.append((id, voorkomen, polygon))
+
+            new_df = spark.createDataFrame(polygons, ['id', 'voorkomen', 'geo_polygon'])
+
+            cond = [
+                df['Objecten:identificatie']['_VALUE'] == new_df.id,
+                df['Objecten:voorkomen']['Historie:Voorkomen']['Historie:voorkomenidentificatie'] == new_df.voorkomen
+            ]
+
+            df = df.join(new_df, cond, 'full').drop('id').drop('voorkomen')
+            new_geo_cols.append('geo_polygon')
+
+        if new_geo_cols:
+            df = df.withColumn('geometry', struct(*new_geo_cols))
+            for col in new_geo_cols:
+                df = df.drop(col)
+
+    df.write \
+        .format('bigquery') \
+        .option('table', f'{DATASET}.{object_type}') \
+        .mode('overwrite') \
+        .save()
 
 if __name__ == '__main__':
     if DOWNLOAD_BAG:
         download_bag_zip()
 
+    transformer = Transformer.from_crs('epsg:28992', 'epsg:4326')
     spark = init_spark_session()
     file_dict = unzip_bag_data()
 
@@ -185,7 +305,6 @@ if __name__ == '__main__':
 
         key_no_digit = ''.join([c for c in key if not c.isdigit()])
         if key_no_digit in object_map.keys():
-            logging.info(key_no_digit)
             files = [f'file://{x}' for x in val]
             joined_files = ','.join(files)
 
@@ -194,24 +313,3 @@ if __name__ == '__main__':
                           'sl-bag-extract:bagStand',
                           'sl-bag-extract:bagObject',
                           'Objecten')
-
-            continue
-
-        if key_no_digit in inonderzoek_map.keys():
-            logging.info(f'InOnderzoek: {key_no_digit}')
-            files = [f'file://{x}' for x in val]
-            joined_files = ','.join(files)
-
-            object_type = inonderzoek_map[key_no_digit]
-            process_table(object_type, joined_files,
-                          'sl-bag-extract:bagStand',
-                          'sl-bag-extract:kenmerkInOnderzoek',
-                          'KenmerkInOnderzoek')
-
-            continue
-
-        #  if key_no_digit in inactief_map.keys():
-            #  continue
-#
-        #  if key_no_digit in nietbag_map.keys():
-            #  continue
