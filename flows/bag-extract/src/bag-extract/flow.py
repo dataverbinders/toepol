ce7b421b615cd883a1719b611ae62d4a33f6d4d5
Marc Zoon <marc.zoon@vintus.nl>
2022-07-11 11:16:19 +0200

    debug

--- flows/bag-extract/src/bag-extract/flow.py
+++ flows/bag-extract/src/bag-extract/flow.py
@@ -27,9 +27,11 @@ schedule = Schedule(clocks=[CronClock("0 0 9 * *")])
 
 load_dotenv()
 
+
 @task
 def print_var(var):
     print(var)
+
 
 with Flow(
     "bag-extract",
@@ -78,34 +80,31 @@ with Flow(
     # Upload XML files to GCS
     paths = generate_blob_directory.map(zipfiles)
     blob_names = generate_blob_names.map(paths, xml_files)
-    print_var.map(blob_names)
 
+    uris = gcs.upload_files_to_gcs(
+        mapped(xml_files), mapped(blob_names), gcp_credentials, gcs_temp_bucket
+    )
 
-    #  uris = gcs.upload_files_to_gcs(
-        #  mapped(xml_files), mapped(paths), gcp_credentials, gcs_temp_bucket
-    #  )
-#
-    #  # Upload files for spark job
-    #  py_file = upload_to_gcs(
-        #  gcp_credentials,
-        #  "/opt/prefect/pyspark/batch_job.py",
-        #  gcs_temp_bucket,
-        #  "bag/dataproc",
-    #  )
-    #  jar_file = upload_to_gcs(
-        #  gcp_credentials,
-        #  "/opt/prefect/pyspark/spark-xml_2.12-0.14.0.jar",
-        #  gcs_temp_bucket,
-        #  "bag/dataproc",
-    #  )
-#
-    #  # Run batch job
-    #  batch_result = submit_batch_job(
-        #  gcp_credentials,
-        #  gcp_region,
-        #  job_config,
-        #  dependencies=[uris, py_file, jar_file],
-    #  )
+    # Upload files for spark job
+    py_file = upload_to_gcs(
+        gcp_credentials,
+        "/opt/prefect/pyspark/batch_job.py",
+        gcs_temp_bucket,
+        "bag/dataproc",
+    )
+    jar_file = upload_to_gcs(
+        gcp_credentials,
+        "/opt/prefect/pyspark/spark-xml_2.12-0.14.0.jar",
+        gcs_temp_bucket,
+        "bag/dataproc",
+    )
 
-if __name__ == "__main__":
-    flow.register("toepol")
+    # Run batch job
+    batch_result = submit_batch_job(
+        gcp_credentials,
+        gcp_region,
+        job_config,
+        dependencies=[uris, py_file, jar_file],
+    )
+
+flow.register("toepol")
