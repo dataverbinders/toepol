daef5f88e863a652377e3962b79e6190a46273aa
Marc Zoon <marc.zoon@vintus.nl>
2022-07-12 08:57:00 +0200

    renamed dataproc import

--- flows/bag-extract/src/util/gcp/dataproc.py
+++ flows/bag-extract/src/util/gcp/dataproc.py
@@ -1,22 +1,22 @@
 import uuid
 
-from google.cloud import dataproc_v1
+from google.cloud import dataproc_v1 as dataproc
 from prefect import task
 
 
 @task
 def submit_batch_job(credentials: dict, region: str, config: dict, **kwargs):
-    client = dataproc_v1.BatchControllerClient().from_service_account_info(
+    client = dataproc.BatchControllerClient().from_service_account_info(
         credentials,
         client_options={
             "api_endpoint": "{}-dataproc.googleapis.com:443".format(region)
         },
     )
 
-    batch = dataproc_v1.Batch(**config)
+    batch = dataproc.Batch(**config)
     batch_id = f"bag-{uuid.uuid1()}"
 
-    request = dataproc_v1.CreateBatchRequest(
+    request = dataproc.CreateBatchRequest(
         batch_id=batch_id,
         parent=f"projects/{credentials['project_id']}/regions/{region}",
         batch=batch,
